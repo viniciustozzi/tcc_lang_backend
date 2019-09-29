1c67a54213b85ca51daef11ceb681699c74065ab
Gabriel <desouza@oculavis.de>
2019-09-29 10:32:45 -0300

    Adds builder

--- TccLangBackend.DB/Repositories/DeckRepository.cs
+++ TccLangBackend.DB/Repositories/DeckRepository.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using TccLangBackend.Core.Deck;
 using TccLangBackend.Core.Flashcard;
 
