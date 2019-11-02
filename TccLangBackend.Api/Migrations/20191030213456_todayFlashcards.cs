bd4fc0a6d52cc35fd3ff17d65556e55ef4a4d4b0
tozzi <tozzivinicius@gmail.com>
2019-11-02 18:58:30 +0100

    Added latest migrations files

---
+++ TccLangBackend.Api/Migrations/20191030213456_todayFlashcards.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TccLangBackend.Api.Migrations
+{
+    public partial class todayFlashcards : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "EasinessFactor",
+                table: "Flashcards",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EasinessFactor",
+                table: "Flashcards");
+        }
+    }
+}
